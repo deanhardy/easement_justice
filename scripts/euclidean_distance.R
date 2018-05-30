512e0baa32ebad740570e7bc32a701bf99707855
Dean Hardy <deanhardy@gmail.com>
2018-05-30 14:55:48 -0400

    Combined all CAs into one df

--- scripts/euclidean_distance.R
+++ scripts/euclidean_distance.R
@@ -24,14 +24,29 @@ YR <- 2010
 ## data import and prepping
 ##############################################################
 
+## import lowcountry region boundary
+lc <- st_read("data/lowcountry.shp") %>%
+  st_transform(utm)
+
 ## import NCED data for low country region in SC & GA
-nced <- st_read("data/nced_lc.shp") %>%
+private <- st_read("data/nced_lc.shp") %>%
   filter(owntype == 'PVT', 
          purpose %in% c('ENV', 'FOR', 'FARM', 'REC')) %>%
-  # filter(gapcat == '2') %>%
-  st_transform(crs = utm)
+  filter(gapcat %in% c('1','2')) %>%
+  mutate(type = "Easement") %>%
+  st_transform(crs = utm) %>%
+  select(type, state, gis_acres, gapcat, geometry) %>%
+  rename(acres = gis_acres, gap = gapcat)
 
 ## import PAD-US data for low country region in SC & GA
+public <- st_read("data/padus_lc.shp") %>%
+  filter(Own_Type %in% c("FED", "STAT"),
+         Category != "Easement") %>%
+  filter(GAP_Sts %in% c('1', '2')) %>%
+  st_transform(crs = utm) %>%
+  select(d_Own_Type, State_Nm, GIS_Acres, GAP_Sts, geometry) %>%
+  rename(type = d_Own_Type, state = State_Nm, acres = GIS_Acres, gap = GAP_Sts)
+
 fed <- st_read("data/padus_lc.shp") %>%
   filter(Own_Type == "FED",
          Category != "Easement") %>%
@@ -41,7 +56,10 @@ state <- st_read("data/padus_lc.shp") %>%
   filter(Own_Type == "STAT",
          Category != "Easement") %>%
   filter(GAP_Sts %in% c('1', '2'))
-  
+
+cons <- rbind(private, public) %>%
+  rowid_to_column()
+
 # ## re-run if census variables need to be changed
 # ## define acs variables of interest
 # acs_vars <- c(white = "B03002_003E", black = "B03002_004E",
@@ -102,28 +120,30 @@ state <- st_read("data/padus_lc.shp") %>%
 bg <- st_read("data/bg_data.geojson") %>%
   st_transform(crs = utm)
 
-# fig <- tm_shape(bg) + 
-#   tm_fill('prop_POC', palette = "Greys",
-#           title = "People of Color") +
-#   tm_shape(nced) + 
-#   tm_fill('owntype', palette = "green", legend.show = FALSE) +
-#   tm_layout(title = "Lowcountry Private Conservation Easements (n = 1196)",
-#             frame = FALSE, 
-#             outer.margins=c(0,0,0,0), 
-#             inner.margins=c(0,0,0,0), asp=0) + 
-#   tm_compass(type = "arrow", size = 5, position = c(0.61, 0.09)) +
-#   tm_scale_bar(breaks = c(0,100), size = 1.2, position= c(0.57, 0.02)) +
-#   tm_legend(position = c(0.8, 0.04),
-#             bg.color = "white",
-#             frame = TRUE,
-#             legend.text.size = 1.2,
-#             legend.title.size = 1.5)
-# fig
-# 
-# tiff('figures/race_pvt_cons.tiff', res = 300, units = 'in',
-#      height = 9, width = 10, compression = 'lzw')
-# fig
-# dev.off()
+fig <- tm_shape(bg) +
+  tm_fill('prop_POC', palette = "Greys",
+          title = "People of Color") +
+  tm_shape(cons) +
+  tm_fill('type', legend.show = TRUE) +
+  tm_shape(lc) +
+  tm_borders(col = 'black') +
+  tm_layout(title = "Lowcountry Conservation Areas (n = 323)",
+            frame = FALSE,
+            outer.margins=c(0,0,0,0),
+            inner.margins=c(0,0,0,0), asp=0) +
+  tm_compass(type = "arrow", size = 5, position = c(0.61, 0.09)) +
+  tm_scale_bar(breaks = c(0,100), size = 1.2, position= c(0.57, 0.02)) +
+  tm_legend(position = c(0.8, 0.04),
+            bg.color = "white",
+            frame = TRUE,
+            legend.text.size = 1.2,
+            legend.title.size = 1.5)
+fig
+
+tiff('figures/cons_race.tiff', res = 300, units = 'in',
+     height = 9, width = 10, compression = 'lzw')
+fig
+dev.off()
 
 
 
@@ -133,7 +153,7 @@ bg <- st_read("data/bg_data.geojson") %>%
 ###################################################
 
 ## euclidean distance buffering
-buf <- nced %>%
+buf <- cons %>%
   st_buffer(dist = 16000) %>%
   mutate(SqKM_BUF = as.numeric(st_area(geometry) / 1e6))
 
@@ -141,10 +161,10 @@ buf <- nced %>%
 int <- as.tibble(st_intersection(buf, bg))
 
 ## proportional area adjustment method
-ncedbuf_bg <- int %>%
+bz_bg <- int %>%
   mutate(SqKMBGinBUF = as.numeric(st_area(geometry) / 1e6)) %>%
   mutate(PercBGinBUF = (SqKMBGinBUF/SqKM_BG)) %>%
-  mutate(total = total * PercBGinBUF,
+  mutate(tot_pop = total * PercBGinBUF,
          white = white * PercBGinBUF, 
          black = black * PercBGinBUF,
          native_american = native_american * PercBGinBUF,
@@ -153,22 +173,22 @@ ncedbuf_bg <- int %>%
          other = other * PercBGinBUF,
          multiracial = multiracial * PercBGinBUF,
          latinx = latinx * PercBGinBUF) %>%
-  group_by(unique_id) %>%
-  select(unique_id, total, white, black, native_american, asian, hawaiian, 
+  group_by(rowid) %>%
+  select(rowid, tot_pop, white, black, native_american, asian, hawaiian, 
          other, multiracial, latinx, SqKM_BUF) %>%
-  summarise(total = sum(total), white = sum(white), black = sum(black), 
+  summarise(tot_pop = sum(tot_pop), white = sum(white), black = sum(black), 
             native_american = sum(native_american),
             asian = sum(asian), hawaiian = sum(hawaiian), other = sum(other),
             multiracial = sum(multiracial), latinx = sum(latinx),
             SqKM_BUF = mean(SqKM_BUF)) %>%
-  mutate(propPOC = (total - white)/total) %>%
-  merge(nced, by = 'unique_id') %>%
+  mutate(propPOC = (tot_pop - white)/tot_pop) %>%
+  merge(cons, by = 'rowid') %>%
   st_as_sf()
 
-df <- ncedbuf_bg %>% 
+df <- bz_bg %>% 
   st_transform(4326)
 
-st_write(ncedbuf_bg,'data/nced_race.geojson', driver = 'geojson')
+st_write(bz_bg,'data/bz_race.geojson', driver = 'geojson')
          
 ## GET EQUATION AND R-SQUARED AS STRING
 ## SOURCE: http://goo.gl/K4yh
