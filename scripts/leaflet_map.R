8c6fc1e7fdc032d95b6e23f773bfc3b71055e34b
Dean Hardy <deanhardy@gmail.com>
2018-06-06 16:12:50 -0400

    added title to leaflet

--- scripts/leaflet_map.R
+++ scripts/leaflet_map.R
@@ -43,6 +43,8 @@ m
 
 ### exploring exporting as html file for offline exploration
 library(htmlwidgets)
-saveWidget(m, file="C:/Users/dhardy/Dropbox/sesync/manuscripts/unpublished/easement_justice/R/easement_justice/docs/lowcountry_conservation.html")
+saveWidget(m, 
+           file="C:/Users/dhardy/Dropbox/sesync/manuscripts/unpublished/easement_justice/R/easement_justice/docs/lowcountry_conservation.html",
+           title = "Lowcountry Conservation Areas")
 
 
