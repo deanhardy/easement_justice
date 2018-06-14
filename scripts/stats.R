1185f541ffbffe5de4e81a4f516bc30a45fc9e17
Dean Hardy <deanhardy@gmail.com>
2018-06-14 16:25:11 -0400

    working on stats

--- scripts/stats.R
+++ scripts/stats.R
@@ -6,7 +6,9 @@ library(multcompView)
 
 ## import data
 df <- st_read('data/bz_data_erp.geojson') %>%
-  st_set_geometry(NULL)
+  st_set_geometry(NULL) %>%
+  mutate(state = as.character(state)) %>%
+  filter(str_detect(state, 'GA|SC'))
 
 #########################################
 ## data exploration
@@ -14,14 +16,24 @@ df <- st_read('data/bz_data_erp.geojson') %>%
 boxplot(propPOC~type, df, notch = TRUE)
 
 df2 <-filter(df, type == 'Easement')
-hist(log(df2$platinx))
+hist(df2$mnmdhhinc)
 
 ####################################
 ## statistical analysis
 ####################################
-# http://rcompanion.org/handbook/G_06.html
-# https://magesblog.com/post/2015-08-04-generalised-linear-models-in-r/
 
+## linear regression model with no mixed effects
+model <- lm(mnmdhhinc ~ type, data = df)
+lsm <- lsmeans(model, ~type)
+cld(lsm, details = TRUE, alpha = 0.05, Letters = letters, adjust = 'bonferroni')
+
+## linear mixed effects model with state jurisdiction as categorial random effect
 model <- lmer(mnmdhhinc ~ type + (type | state), data = df)
 lsm <- lsmeans(model, ~type)
 cld(lsm, details = TRUE, alpha = 0.05, Letters = letters, adjust = 'bonferroni')
+
+## note that I'm finding significant differences with state as random effect, but not without it
+
+## some useful resources
+# http://rcompanion.org/handbook/G_06.html
+# https://magesblog.com/post/2015-08-04-generalised-linear-models-in-r/
