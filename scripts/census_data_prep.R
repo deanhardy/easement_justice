a8d91e6199927db65af64bf9db2080a339d4a610
Dean Hardy <deanhardy@gmail.com>
2018-06-24 15:11:22 -0400

    added "edge states" to census download

--- scripts/census_data_prep.R
+++ scripts/census_data_prep.R
@@ -76,7 +76,55 @@ sc <- get_acs(geography = "block group",
   dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
                 other, multiracial, latinx, medhhinc, agghhinc, hu, mnhhinc)
 
-bg <- rbind(ga, sc) %>%
+al <- get_acs(geography = "block group",
+              variables = c(white = "B03002_003E", black = "B03002_004E",
+                            native_american = "B03002_005E", asian = "B03002_006E",
+                            hawaiian = "B03002_007E", other = "B03002_008E",
+                            multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
+                            medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E"),
+              state = 'Alabama',
+              year = YR,
+              output = 'wide',
+              geometry = TRUE,
+              keep_geo_vars = TRUE) %>%
+  st_transform(crs = alb) %>%
+  mutate(sqkm_bg = as.numeric(st_area(geometry)) / 1e6, mnhhinc = agghhinc/hu) %>%
+  dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
+                other, multiracial, latinx, medhhinc, agghhinc, hu, mnhhinc)
+
+fl <- get_acs(geography = "block group",
+              variables = c(white = "B03002_003E", black = "B03002_004E",
+                            native_american = "B03002_005E", asian = "B03002_006E",
+                            hawaiian = "B03002_007E", other = "B03002_008E",
+                            multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
+                            medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E"),
+              state = 'Florida',
+              year = YR,
+              output = 'wide',
+              geometry = TRUE,
+              keep_geo_vars = TRUE) %>%
+  st_transform(crs = alb) %>%
+  mutate(sqkm_bg = as.numeric(st_area(geometry)) / 1e6, mnhhinc = agghhinc/hu) %>%
+  dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
+                other, multiracial, latinx, medhhinc, agghhinc, hu, mnhhinc)
+
+nc <- get_acs(geography = "block group",
+              variables = c(white = "B03002_003E", black = "B03002_004E",
+                            native_american = "B03002_005E", asian = "B03002_006E",
+                            hawaiian = "B03002_007E", other = "B03002_008E",
+                            multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
+                            medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E"),
+              state = 'North Carolina',
+              year = YR,
+              output = 'wide',
+              geometry = TRUE,
+              keep_geo_vars = TRUE) %>%
+  st_transform(crs = alb) %>%
+  mutate(sqkm_bg = as.numeric(st_area(geometry)) / 1e6, mnhhinc = agghhinc/hu) %>%
+  dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
+                other, multiracial, latinx, medhhinc, agghhinc, hu, mnhhinc)
+
+bg <- rbind(ga, sc, al, fl, nc) %>%
   mutate(prop_POC = 1 - (white/total))
 
 
