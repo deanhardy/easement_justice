763ac795f1df12298e2d4ddd5de01f0582afb110
Dean Hardy <deanhardy@gmail.com>
2018-06-29 13:41:28 -0400

    for loop works now with multiple states

--- scripts/census_data_prep.R
+++ scripts/census_data_prep.R
@@ -7,23 +7,18 @@ options(tigris_use_cache = TRUE)
 library(sf)
 
 ## define variables
-## Albers Conic Equal Area projection
-## http://spatialreference.org/ref/sr-org/albers-conic-equal-area-for-florida-and-georgia/
-alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
-
-# nad83 <- "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs"
-geometry = st_sfc(lapply(1:1, function(x) st_geometrycollection()))
-bg <- st_sf(data.frame(geometry = geometry), crs = alb)
+alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" ## http://spatialreference.org/ref/sr-org/albers-conic-equal-area-for-florida-and-georgia/
+bg <- NULL
 YR <- 2016
-ST <- c('AL')
+ST <- c('AL', 'GA', 'FL', 'SC', 'NC')
 var = c(white = "B03002_003E", black = "B03002_004E",
         native_american = "B03002_005E", asian = "B03002_006E",
         hawaiian = "B03002_007E", other = "B03002_008E",
         multiracial = "B03002_009E", latinx = "B03002_012E", total = "B03002_001E",
         medhhinc = "B19049_001E", agghhinc = "B19025_001E", hu = "B25001_001E")
  
-# ## define decennial variables of interest
 # all_vars <- load_variables(2016, 'acs5', cache = TRUE)
+# ## define decennial variables of interest
 # dec_vars <- c(white = "P0050003", black = "P0050004",
 #               native_american = "P0050005", asian = "P0050006",
 #               hawaiian = "P0050007", other = "P0050008",
@@ -48,20 +43,21 @@ for(i in 1:length(ST)) {
   OUT <- get_acs(geography = "block group",
               variables = var,
               state = ST[[i]],
-              county = 'Jefferson',
               year = YR,
               output = 'wide',
               geometry = TRUE,
               keep_geo_vars = TRUE) %>%
-    st_transform(alb)
-  bg <- bind_rows(OUT, bg)
+    data.frame()  
+    bg <- rbind(OUT, bg)
 }
 
 bg2 <- bg %>%
+  st_sf() %>%
+  st_transform(alb) %>%
   mutate(sqkm_bg = as.numeric(st_area(geometry)) / 1e6, mnhhinc = agghhinc/hu,
-         prop_POC = 1 - (white/total)) %>%
+         propPOC = 1 - (white/total)) %>%
   dplyr::select(GEOID, ALAND, AWATER, sqkm_bg, total, white, black, native_american, asian, hawaiian,
-                other, multiracial, latinx, medhhinc, agghhinc, hu, mnhhinc)
+                other, multiracial, latinx, propPOC, medhhinc, agghhinc, hu, mnhhinc)
 
 ## export census data
 bg2 %>% st_transform(crs = 4326) %>%
