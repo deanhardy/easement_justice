3766a81ab9420a74e2015785290657cfbf2c2bf6
Dean Hardy <deanhardy@gmail.com>
2018-09-10 17:14:15 -0400

    added mapping of overlapping datasets

--- scripts/cons_lands_prep.R
+++ scripts/cons_lands_prep.R
@@ -11,6 +11,15 @@ alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +el
 #define data directory
 datadir <- file.path('C:/Users/dhardy/Dropbox/r_data/cons_lands')
 
+## import "lowcountry" regions
+t1 <- st_read(file.path(datadir, "lc_tier1.shp")) %>%
+  st_transform(utm)
+t2 <- st_read(file.path(datadir, "lc_tier2.shp")) %>%
+  st_transform(utm)
+t3 <- st_read(file.path(datadir, "lc_tier3.shp")) %>%
+  st_transform(utm)
+
+
 ## import protected SC-TNC for SC coastal plain region (tier 3)
 ## DO NOT SHARE DATA
 tnc <- st_read(file.path(datadir, "tnc.shp")) %>%
@@ -26,8 +35,11 @@ tnc <- st_read(file.path(datadir, "tnc.shp")) %>%
          orig_id = ORIG_FID,
          sitename = SiteName,
          purpose = PurposeCde,
-         access = PubAccess) 
-  #mutate(access = ifelse(access == 'Closed', 'XA', access))
+         access = PubAccess)  %>%
+  mutate(access = ifelse(access %in% c('Closed', 'No', 'NO'),'XA', 
+                          ifelse(access %in% c(NA, 363.138382858769), 'UK', 
+                                 ifelse(access %in% c('Restricted', 'Limited', 'Limited Access'), 'RA',
+                                        ifelse(access %in% c('Open', 'Public Access', 'Yes'), 'OA', NA)))))
 
 ## import NCED data for coastal plain (lc tier 3) region in SC & GA
 nced <- st_read(file.path(datadir, "nced.shp")) %>%
@@ -59,18 +71,34 @@ padus <- st_read(file.path(datadir, "padus.shp")) %>%
          access = Access,
          orig_id = ORIG_FID)
 
+dat <- rbind(nced, padus, tnc)
 
-ggplot() + 
-  geom_sf(data = tnc, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = TRUE) + 
-  geom_sf(data = padus, aes(color = NULL, fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE) + 
-  geom_sf(data = nced, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE)
+st_write(dat, file.path(datadir, 'cons_lands.shp'), driver = 'ESRI Shapefile')
 
-tm_shape(padus) +
-  tm_fill(col = 'blue', alpha = 0.5)+
-  tm_shape(nced) + 
-  tm_fill(col = 'yellow', alpha = 0.5) +
-  tm_shape(tnc) + 
-  tm_fill(col = 'red', alpha = 0.5)
+fig <- tm_shape(t3) + tm_fill(col = 'grey95') +
+  tm_shape(t2) + tm_borders(col = 'grey65') +
+  tm_shape(t1) + tm_borders(col = 'grey40') +
+  tm_shape(dat) + 
+  tm_fill(col = 'source', alpha = 0.5, palette = c('red', 'green', 'yellow'))
+
+tiff('figures/conslands_by_source.tiff', compression = 'lzw', units = 'in',
+     height = 5, width = 7, res = 300)
+fig
+dev.off()
+  
+  
+# ggplot() + 
+#   geom_sf(data = tnc, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = TRUE) + 
+#   geom_sf(data = padus, aes(color = NULL, fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE) + 
+#   geom_sf(data = nced, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE)
+
+# tm_shape(padus) +
+#   tm_fill(col = 'blue', alpha = 0.5)+
+#   tm_shape(nced) + 
+#   tm_fill(col = 'yellow', alpha = 0.5) +
+#   tm_shape(tnc) + 
+#   tm_fill(col = 'red', alpha = 0.5)
+
 
   
     
