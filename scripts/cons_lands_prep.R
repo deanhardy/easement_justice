ba3293738f12747decb66930e6b99589e46a070d
Dean Hardy <deanhardy@gmail.com>
2018-09-10 16:21:30 -0400

    tidying data via variable selection and naming of 3 data sets

--- scripts/cons_lands_prep.R
+++ scripts/cons_lands_prep.R
@@ -10,46 +10,69 @@ alb <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-84 +x_0=0 +y_0=0 +el
 
 #define data directory
 datadir <- file.path('C:/Users/dhardy/Dropbox/r_data/cons_lands')
-  
-## import NCED data for coastal plain (lc tier 3) region in SC & GA
-nced <- st_read(file.path(datadir, "nced.shp")) %>%
-  st_transform(crs = utm) %>%
-  filter(owntype == 'PVT') %>%
-  mutate(source = 'nced')
-  #        purpose %in% c('ENV', 'FOR', 'FARM', 'REC')) %>%
-  # #  filter(gapcat %in% c('1','2')) %>%
-  # mutate(type = "Easement") %>%
-  # dplyr::select(type, state, sitename, esmthldr, gis_acres, gapcat, purpose, geometry) %>%
-  # rename(management = esmthldr, 
-  #        acres = gis_acres, 
-  #        gap = gapcat)
-
-## import PAD-US data
-padus <- st_read(file.path(datadir, "padus.shp")) %>%
-  st_transform(crs = utm) %>%
-  filter(Own_Type != "PVT", Own_Type != "NGO") %>%
-  mutate(source = 'padus')
-  #        Category != "Easement") %>%
-  # filter(GAP_Sts %in% c('1', '2')) %>%
-  # mutate(purpose = 'NA') %>%
-  # dplyr::select(d_Own_Type, State_Nm, Unit_Nm, d_Mang_Nam, GIS_Acres, GAP_Sts, purpose, geometry) %>%
-  # rename(type = d_Own_Type, 
-  #        state = State_Nm, 
-  #        sitename = Unit_Nm, 
-  #        management = d_Mang_Nam, 
-  #        acres = GIS_Acres, 
-  #        gap = GAP_Sts)
 
 ## import protected SC-TNC for SC coastal plain region (tier 3)
 ## DO NOT SHARE DATA
 tnc <- st_read(file.path(datadir, "tnc.shp")) %>%
   st_transform(crs = utm) %>%
-  filter(OwnType %in% c("Private", "NGO", "<NA>")) %>%
-  mutate(source = 'tnc')
+  # filter(OwnType %in% c("Private", "NGO", "<NA>")) %>%
+  mutate(source = 'tnc', sqkm = as.numeric(st_area(geometry) / 1e6), 
+         purpose = 'NA', state = 'SC', gap = 'NA') %>%
+  dplyr::select(OwnType, HoldType, EsmtHldr, SiteName, PubAccess, state,
+                sqkm, gap, PurposeCde, ORIG_FID, ecorg_tier, source, geometry) %>%
+  rename(owntype = OwnType,
+         mgmttype = HoldType,
+         management = EsmtHldr,
+         orig_id = ORIG_FID,
+         sitename = SiteName,
+         purpose = PurposeCde,
+         access = PubAccess) 
+  #mutate(access = ifelse(access == 'Closed', 'XA', access))
+
+## import NCED data for coastal plain (lc tier 3) region in SC & GA
+nced <- st_read(file.path(datadir, "nced.shp")) %>%
+  st_transform(crs = utm) %>%
+  # filter(owntype %in% c('PVT', 'NGO')) %>%
+  mutate(source = 'nced', sqkm = as.numeric(st_area(geometry) / 1e6)) %>%
+  dplyr::select(owntype, eholdtype, esmthldr, sitename, pubaccess, state, 
+                sqkm, gapcat, purpose, ORIG_FID, ecorg_tier, source, geometry) %>%
+  rename(management = esmthldr, 
+        gap = gapcat,
+        access = pubaccess,
+        orig_id = ORIG_FID,
+        mgmttype = eholdtype)
+
+## import PAD-US data
+padus <- st_read(file.path(datadir, "padus.shp")) %>%
+  st_transform(crs = utm) %>%
+  # filter(Own_Type != "PVT", Own_Type != "NGO") %>%
+  mutate(source = 'padus', sqkm = as.numeric(st_area(geometry) / 1e6), 
+         purpose = 'NA') %>%
+  dplyr::select(Own_Type, Mang_Type, Loc_Mang, Unit_Nm, Access, State_Nm,
+                sqkm, GAP_Sts, purpose, ORIG_FID, ecorg_tier, source, geometry) %>%
+  rename(owntype = Own_Type,
+         state = State_Nm,
+         sitename = Unit_Nm,
+         management = Loc_Mang,
+         mgmttype = Mang_Type,
+         gap = GAP_Sts,
+         access = Access,
+         orig_id = ORIG_FID)
+
 
 ggplot() + 
   geom_sf(data = tnc, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = TRUE) + 
   geom_sf(data = padus, aes(color = NULL, fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE) + 
   geom_sf(data = nced, aes(fill = source), lwd = 0, alpha = 0.3, inherit.aes = FALSE)
+
+tm_shape(padus) +
+  tm_fill(col = 'blue', alpha = 0.5)+
+  tm_shape(nced) + 
+  tm_fill(col = 'yellow', alpha = 0.5) +
+  tm_shape(tnc) + 
+  tm_fill(col = 'red', alpha = 0.5)
+
+  
+    
 
 
