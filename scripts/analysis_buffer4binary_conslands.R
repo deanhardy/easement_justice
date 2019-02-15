7623f5bb07fdc3834aea695132c12b6c7fd71ab5
Dean Hardy <dhardy@sesync.org>
2019-02-15 10:45:27 -0500

    added interactive map for buffer zones

--- scripts/analysis_buffer4binary_conslands.R
+++ scripts/analysis_buffer4binary_conslands.R
@@ -28,6 +28,7 @@ pvt <- dat %>%
   filter(ecorg_tier == 1 & conscat == 'Private') %>%
   st_union()
 
+#### still working on this; some of the combos produce same result
 ## private and public reserves in close proximity via buffering
 ## nested for loop
 for(i in PERC) {
@@ -52,24 +53,47 @@ for(i in PERC) {
   }
 }
 
-# CL <- rbind(pub, pvt)
-# cl_buf <- NULL
-# ## working on nested for loop
-# for(x in 1:length(CL)) {
-# for(i in PERC) {
-# for(j in DIST) {
-#     OUT <- cl_buf %>%
-#       st_buffer(., dist = i * j) %>%
-#       st_cast("POLYGON") %>%
-#       data.frame() %>%
-#       mutate(buf_m = i * j, conscat = x)
-#     cl_buf <- rbind(OUT, cl_buf)
-# }
-# }
-# }
-
 cl_buf <- rbind(pvt_buf, pub_buf)
 
 table(cl_buf$buf_m, cl_buf$conscat)
 
 st_write(cl_buf, file.path(datadir, 'conslands_er1_bufs.shp'), driver = 'ESRI Shapefile')
+
+#########################
+##
+#########################
+
+cl <- st_transform(dat, 4326)
+buf <- cl_buf %>% st_as_sf() %>% st_transform(4326)
+
+pal <- colorFactor(rainbow(7), buf$buf_m)
+
+m <- leaflet() %>%
+  addTiles(group = "Open Street Map") %>%
+  setView(lng = -81, lat = 33, zoom = 7) %>%
+  addSearchOSM(options = searchOptions(autoCollapse = TRUE, minLength = 2)) %>%
+  addPolygons(data = buf, 
+              color = ~pal(buf$buf_m),
+              fillOpacity = 0.5,
+              weight = 1, 
+              group = "Buffer Zones") %>%
+  addPolygons(data = cl,
+              group = "Conservation Areas",
+              fillColor = cl$conscat,
+              fillOpacity = 0.5,
+              weight = 1) %>%
+  addLayersControl(baseGroups = c("Open Street Map"), 
+                   overlayGroups = c("Conservation Areas", "Buffer Zones"),
+                   options = layersControlOptions(collapsed = TRUE)) %>%
+  addLegend("bottomright",
+            pal = pal,
+            values = buf$buf_m,
+            title = "Buffer Zone") %>%
+  addScaleBar("bottomright")
+m
+
+## exporting as html file for exploration
+library(htmlwidgets)
+saveWidget(m, 
+           file="/Users/dhardy/Dropbox/r_data/cons_lands/buffer_zones.html",
+           title = "Buffer Zones")
