1205a8d4db9931a97fe0400e51dc486220993a3f
Dean Hardy <dhardy@sesync.org>
2020-03-15 22:43:01 -0400

    added emedhhinc to state data for lowcountry

--- scripts/analysis_lowcountry_demographic.R
+++ scripts/analysis_lowcountry_demographic.R
@@ -65,10 +65,10 @@ percBGinAOI <- int %>%
 bg_for_emed <- percBGinAOI %>%
   data.frame() %>%
   mutate(GEOID = as.character(GEOID)) %>%
-  select(statefp, perc_bginAOI)
+  select(statefp, GEOID, perc_bginAOI)
 
 ## demographic analysis of cons area ben zones
-bz_geog <- percBGinAOI %>%
+AOI_geog <- percBGinAOI %>%
   mutate(tot_pop = total * perc_bginAOI,
          white = white * perc_bginAOI, 
          black = black * perc_bginAOI,
@@ -85,7 +85,115 @@ bz_geog <- percBGinAOI %>%
   mutate(pwhite = round(white/tot_pop, 2), pblack = round(black/tot_pop, 2), pother = round(other/tot_pop, 2), 
          platinx = round(latinx/tot_pop, 2), popden = round(tot_pop/sqkm_land, 2), propPOC = round(1 - pwhite, 2),
          mnhhinc = round(agghhinc/hu, 0)) %>%
-  merge(AOI, by = 'statefp') %>%
-  dplyr::select(rowid, conscat, bzone_m, buf_m, tot_pop, popden, sqkm_bz, pland, pwhite, white, pblack, black, pother, other, 
+  merge(AOI) %>%
+  dplyr::select(statefp, tot_pop, popden, pwhite, white, pblack, black, pother, other, 
                 platinx, latinx, propPOC, hu, mnhhinc, geometry) %>%
   st_as_sf()
+
+
+#########################################################
+## estimate median household incomes within buffer zones
+#########################################################
+## this section of code calculates an estimated median from grouped (aka binned) household income data 
+## within an area that overlaps several block groups
+
+## good explanation for how this works mathematically and upon which the function below is based
+## https://www.mathsisfun.com/data/frequency-grouped-mean-median-mode.html
+
+## download hh income distribution tables for block groups & label bins
+for(i in 1:length(ST)) {
+  OUT <- get_acs(geography = "block group",
+                 table = 'B19001',
+                 state = ST[[i]],
+                 year = YR) %>%
+    select(-NAME, -moe) %>%
+    rename(households = estimate) %>%
+    filter(variable != 'B19001_001') %>%
+    mutate(bin_min = ifelse(variable == 'B19001_002', 0, 
+                            ifelse(variable == 'B19001_003', 10000, 
+                                   ifelse(variable == 'B19001_004', 15000,
+                                          ifelse(variable == 'B19001_005', 20000,
+                                                 ifelse(variable == 'B19001_006', 25000,
+                                                        ifelse(variable == 'B19001_007', 30000,
+                                                               ifelse(variable == 'B19001_008', 35000,
+                                                                      ifelse(variable == 'B19001_009', 40000,
+                                                                             ifelse(variable == 'B19001_010', 45000,
+                                                                                    ifelse(variable == 'B19001_011', 50000,
+                                                                                           ifelse(variable == 'B19001_012', 60000,
+                                                                                                  ifelse(variable == 'B19001_013', 75000,
+                                                                                                         ifelse(variable == 'B19001_014', 100000,
+                                                                                                                ifelse(variable == 'B19001_015', 125000,
+                                                                                                                       ifelse(variable == 'B19001_016', 150000,
+                                                                                                                              ifelse(variable == 'B19001_017', 200000, variable)))))))))))))))),
+           bin_max = ifelse(variable == 'B19001_002', 9999, 
+                            ifelse(variable == 'B19001_003', 14999, 
+                                   ifelse(variable == 'B19001_004', 19999,
+                                          ifelse(variable == 'B19001_005', 24999,
+                                                 ifelse(variable == 'B19001_006', 29999,
+                                                        ifelse(variable == 'B19001_007', 34999,
+                                                               ifelse(variable == 'B19001_008', 39999,
+                                                                      ifelse(variable == 'B19001_009', 44999,
+                                                                             ifelse(variable == 'B19001_010', 49999,
+                                                                                    ifelse(variable == 'B19001_011', 59999,
+                                                                                           ifelse(variable == 'B19001_012', 74999,
+                                                                                                  ifelse(variable == 'B19001_013', 99999,
+                                                                                                         ifelse(variable == 'B19001_014', 124999,
+                                                                                                                ifelse(variable == 'B19001_015', 149999,
+                                                                                                                       ifelse(variable == 'B19001_016', 199999,
+                                                                                                                              ifelse(variable == 'B19001_017', NA, variable))))))))))))))))) %>%
+    mutate(interval = paste(bin_min, bin_max, sep = "-"))
+  
+  gm <- rbind(gm, OUT)
+}
+
+## define function following stackoverflow post
+# https://stackoverflow.com/questions/18887382/how-to-calculate-the-median-on-grouped-dataset
+## but revised per variables from 
+# https://www.mathsisfun.com/data/frequency-grouped-mean-median-mode.html
+## B modified to account for when median group is the 0-9999 bin
+GMedian <- function(frequencies, intervals, sep = NULL, trim = NULL) {
+  # If "sep" is specified, the function will try to create the 
+  #   required "intervals" matrix. "trim" removes any unwanted 
+  #   characters before attempting to convert the ranges to numeric.
+  if (!is.null(sep)) {
+    if (is.null(trim)) pattern <- ""
+    else if (trim == "cut") pattern <- "\\[|\\]|\\(|\\)"
+    else pattern <- trim
+    intervals <- sapply(strsplit(gsub(pattern, "", intervals), sep), as.numeric)
+  }
+  
+  cf <- cumsum(frequencies)
+  Midrow <- findInterval(max(cf)/2, cf) + 1
+  L <- intervals[1, Midrow]      # lower class boundary of the group containing the median 
+  w <- diff(intervals[, Midrow]) # width of median class
+  G <- frequencies[Midrow]       # frequency of median class
+  B <- ifelse(Midrow > 1, cf[Midrow - 1], as.vector(0))  # cumulative frequency of the groups before median group
+  n_2 <- max(cf)/2               # total observations divided by 2
+  
+  unname(L + (n_2 - B)/G * w)
+}
+
+bg2 <- gm %>%
+  mutate(statefp = as.numeric(str_extract(GEOID, "^.{2}"))) %>%
+  left_join(bg_for_emed, by = "statefp") %>%
+  filter(perc_bginAOI != 'NA') %>%
+  mutate(eHH = households * perc_bginAOI) %>%
+  group_by(statefp, variable) %>%
+  summarise(interval = interval[[1]], eHH = sum(eHH), households = sum(households)) %>%
+  summarise(gmedian = GMedian(eHH, interval, sep = "-", trim = 'cut'))
+
+## import gmedian estimates for hh income
+emed <- bg2 %>%
+  rename(emedhhinc = gmedian) %>%
+  mutate(emedhhinc = round(emedhhinc, 0))
+
+## merge emedian hh income with other demographic data
+df <- AOI_geog %>% 
+  merge(emed, by = "statefp") %>%
+  st_transform(4326) 
+
+## export ONLY attribute data
+df %>%
+  st_set_geometry(NULL) %>%
+  write.csv(file.path(datadir, 'lowcountry-by-state_data.csv'), row.names = FALSE)
+
