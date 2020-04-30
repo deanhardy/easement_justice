74c310f5c28ad2cbd4d035f56003fa56318d494a
“Dean <deanhardy@gmail.com>
2020-04-30 17:22:21 -0400

    interactive map updates

--- scripts/mapping/maps_interactive.R
+++ scripts/mapping/maps_interactive.R
@@ -28,7 +28,8 @@ bz <- st_read(file.path(datadir, 'cabz.geojson')) %>%
 
 ## import block group data
 bg <- st_read(file.path(datadir, 'bg_demg.geojson'))%>%
-  filter(st_intersects(t1, ., sparse = F))
+  filter(st_intersects(t1, ., sparse = F)) %>%
+  mutate(popden = total/sqkm_bg)
 
 # set color schemes
 clpal <- colorFactor(c('darkorchid4', 'darkgreen'), cl$conscat)
@@ -36,13 +37,16 @@ bufpal <- colorFactor(c('darkorchid1', 'green'), buf$conscat)
 bzpal <- colorFactor(c('darkorchid1', 'green'), bz$conscat)
 # bgpal <- colorFactor(gray.colors(5, start = 0.3, end = 0.9, gamma = 1, rev = TRUE), bg$propPOC)
 bgpal <- colorQuantile("Greens", bg$propPOC, n = 3)
+bgdenpal <- colorQuantile("Reds", bg$popden, n = 3)
 bgincpal <- colorQuantile("Blues", bg$medhhinc, n = 3)
+bgmhvpal <- colorQuantile("Oranges", bg$mhv, n = 3)
+
 
 # make leaflet map
 m <- leaflet() %>%
   addTiles(group = "Open Street Map") %>%
-  addTiles(attribution = '<a href="https://www.conservationeasement.us/"> | NCED</a>') %>%
-  addProviderTiles(providers$Esri.WorldImagery, group = "Esri World Imagery") %>%
+  # addTiles(attribution = '<a href="https://www.conservationeasement.us/"> | NCED</a>') %>%
+  # addProviderTiles(providers$Esri.WorldImagery, group = "Esri World Imagery") %>%
   setView(lng = -81, lat = 32.4, zoom = 8) %>%
   addSearchOSM(options = searchOptions(autoCollapse = TRUE, minLength = 2)) %>%
   addPolylines(data = t1,
@@ -60,7 +64,7 @@ m <- leaflet() %>%
                             "White (%):", 100*(bg$white/bg$total), "<br>",
                             "Median HH Income (US$):", round(bg$medhhinc, 0), "<br>",
                             "Mean HH Income (US$):", bg$mnhhinc),
-              group = "US Census (Race)",
+              group = "US Census (POC)",
               color = 'black',
               fillColor = ~bgpal(bg$propPOC),
               fillOpacity = 1,
@@ -80,6 +84,40 @@ m <- leaflet() %>%
               group = "US Census (Income)",
               color = 'black',
               fillColor = ~bgincpal(bg$medhhinc),
+              fillOpacity = 1,
+              weight = 0.5) %>%
+  addPolygons(data = bg,
+              popup = paste("<strong>Block Group Info</strong>", "<br>",
+                            "Land (%):", 100*buf$pland, "<br>",
+                            "Total Population: ", buf$tot_pop, "<br>",
+                            "Population Density (per KM^2): ", bg$total/bg$sqkm_bg, "<br>",
+                            "People of Color (%):", 100*buf$propPOC, "<br>",
+                            "Black (%):", 100*(bg$black/bg$total), "<br>",
+                            # "Other race (%):", 100*(bg$other/bg$total)), "<br>",
+                            "Latinx (%):", 100*(bg$latinx/bg$total), "<br>",
+                            "White (%):", 100*(bg$white/bg$total), "<br>",
+                            "Median HH Income (US$):", round(bg$medhhinc, 0), "<br>",
+                            "Mean HH Income (US$):", bg$mnhhinc),
+              group = "US Census (MHV)",
+              color = 'black',
+              fillColor = ~bgmhvpal(bg$mhv),
+              fillOpacity = 1,
+              weight = 0.5) %>%
+  addPolygons(data = bg,
+              popup = paste("<strong>Block Group Info</strong>", "<br>",
+                            "Land (%):", 100*buf$pland, "<br>",
+                            "Total Population: ", buf$tot_pop, "<br>",
+                            "Population Density (per KM^2): ", bg$total/bg$sqkm_bg, "<br>",
+                            "People of Color (%):", 100*buf$propPOC, "<br>",
+                            "Black (%):", 100*(bg$black/bg$total), "<br>",
+                            # "Other race (%):", 100*(bg$other/bg$total)), "<br>",
+                            "Latinx (%):", 100*(bg$latinx/bg$total), "<br>",
+                            "White (%):", 100*(bg$white/bg$total), "<br>",
+                            "Median HH Income (US$):", round(bg$medhhinc, 0), "<br>",
+                            "Mean HH Income (US$):", bg$mnhhinc),
+              group = "US Census (Density)",
+              color = 'black',
+              fillColor = ~bgdenpal(bg$popden),
               fillOpacity = 1,
               weight = 0.5) %>%
   # addPolylines(data = bz,
@@ -121,19 +159,29 @@ m <- leaflet() %>%
               color = 'black',
               fillOpacity = 1,
               weight = 1) %>%
-  addLayersControl(baseGroups = c("Open Street Map", "Esri World Imagery", "US Census (Race)",  "US Census (Income)"), 
+  addLayersControl(baseGroups = c("US Census (POC)", "US Census (Income)", "US Census (MHV)", "US Census (Density)"), 
       overlayGroups = c("Conservation Buffer Zones", "Conservation Reserves", "Beneficiary Zones"),
       options = layersControlOptions(collapsed = FALSE)) %>%
   addLegend("bottomright",
             pal = bgpal,
             values = bg$propPOC,
-            group = "US Census (Race)",
+            group = "US Census (POC)",
             title = "People of Color (%)") %>%
   addLegend("bottomright",
+            pal = bgdenpal,
+            values = bg$popden,
+            group = "US Census (Density)",
+            title = "Population Density") %>%
+  addLegend("bottomleft",
             pal = bgincpal,
             values = bg$medhhinc,
             group = "US Census (Income)",
             title = "Median Household Income") %>%
+  addLegend("bottomleft",
+            pal = bgmhvpal,
+            values = bg$mhv,
+            group = "US Census (MHV)",
+            title = "Median House Value") %>%
   addLegend("bottomright",
             pal = clpal,
             values = cl$conscat,
