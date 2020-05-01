664e66129112887ca585b7c603856bdd1ae5996a
“Dean <deanhardy@gmail.com>
2020-05-01 17:13:17 -0400

    tweaked tables

--- scripts/analysis_comparison_to_region.R
+++ scripts/analysis_comparison_to_region.R
@@ -16,47 +16,48 @@ library(data.table)
 datadir <- file.path('/Users/dhardy/Dropbox/r_data/easement-justice')
 
 ## import data
-st <- read.csv(file.path(datadir, 'st_data.csv')) %>%
-  filter(GEOID == 13 | GEOID == 45) %>%
-  mutate(pwhite = white/total, pblack = black/total, platinx = latinx/total, 
-         pother = (native_american+asian+hawaiian+other+multiracial)/total, 
-         propPOC = (total-white)/total,
-         cat = 'State') %>%
-  rename(tot_pop = total, statefp = GEOID) %>%
-  select(cat, statefp, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc)
+# st <- read.csv(file.path(datadir, 'st_data.csv')) %>%
+#   filter(GEOID == 13 | GEOID == 45) %>%
+#   mutate(pwhite = white/total, pblack = black/total, platinx = latinx/total, 
+#          pother = (native_american+asian+hawaiian+other+multiracial)/total, 
+#          propPOC = (total-white)/total,
+#          cat = 'State') %>%
+#   rename(tot_pop = total, statefp = GEOID) %>%
+#   select(cat, statefp, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc)
 
 ## GA and SC reserve data by state
-cl_demg <- read.csv(file.path(datadir, 'cl_demg_data.csv')) %>%
+cl_demg <- read.csv(file.path(datadir, 'cl_buf_demg_data.csv')) %>%
   filter(buf_m == 320 & bzone_m == 16000 & statefp != 'NA') %>% 
   rename(cat = conscat, medhhinc = emedhhinc) %>%
-  select(cat, statefp, sqkm_bz, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
+  mutate(urban = if_else(urban == 'yes', 1, 0)) %>%
+  select(cat, statefp, sqkm_bz, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
   group_by(cat, statefp) %>%
-  summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
+  summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = sum(urban), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
             propPOC = mean(propPOC), medhhinc = mean(medhhinc), ) %>%
   as.data.frame()
 
 ## combine GA and SC reserve data
-cl_demg_entire <- read.csv(file.path(datadir, 'cl_demg_data.csv')) %>%
+cl_demg_entire <- read.csv(file.path(datadir, 'cl_buf_demg_data.csv')) %>%
   filter(buf_m == 320 & bzone_m == 16000 & statefp != 'NA') %>% 
   rename(cat = conscat, medhhinc = emedhhinc) %>%
-  select(cat, statefp, sqkm_bz, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
+  mutate(urban = if_else(urban == 'yes', 1, 0)) %>%
+  select(cat, statefp, sqkm_bz, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
   group_by(cat) %>%
-  summarise(statefp = 'Lowcountry', sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
+  summarise(statefp = 'Lowcountry', sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = sum(urban), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
             propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
   as.data.frame()
 
 ## GA and SC lowcountry data by state
 lc <- read.csv(file.path(datadir, 'lowcountry-by-state_data.csv')) %>%
   rename(medhhinc = emedhhinc, sqkm = sqkm_land) %>%
-  mutate(cat = 'Lowcountry') %>%
-  select(cat, statefp, sqkm, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc)
+  mutate(cat = 'Lowcountry', urban = NA) %>%
+  select(cat, statefp, sqkm, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc)
 
 ## combine GA and SC lowcountry data
 lc_entire <- read.csv(file.path(datadir, 'lowcountry_data.csv')) %>%
   rename(medhhinc = emedhhinc, sqkm = sqkm_land) %>%
-  mutate(cat = 'Lowcountry', statefp = 'Lowcountry') %>%
-  select(cat, statefp, sqkm, tot_pop, pwhite, pblack, pother, platinx, propPOC, medhhinc)
-
+  mutate(cat = 'Lowcountry', statefp = 'Lowcountry', urban = NA) %>%
+  select(cat, statefp, sqkm, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc)
 
 
 df2 <- rbind(cl_demg, cl_demg_entire)
@@ -72,6 +73,7 @@ df5 <- df4 %>%
                          if_else(statefp == 13, 'Georgia', 'Lowcountry')),
          sqkm = round(sqkm, 0),
          tot_pop = round(tot_pop, 0),
+         popden = round(popden, 0),
          pwhite = round(pwhite, 2),
          pblack = round(pblack, 2),
          platinx = round(platinx, 2),
