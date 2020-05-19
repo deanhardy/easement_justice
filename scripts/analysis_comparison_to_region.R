d84beb4a17ee260914a2e5fd5bd2cbe8829f0087
“Dean <deanhardy@gmail.com>
2020-05-19 16:21:29 -0400

    updating tables and adding SDs to new script of tables

--- scripts/analysis_comparison_to_region.R
+++ scripts/analysis_comparison_to_region.R
@@ -29,11 +29,16 @@ datadir <- file.path('/Users/dhardy/Dropbox/r_data/easement-justice')
 cl_demg <- read.csv(file.path(datadir, 'cl_buf_demg_data.csv')) %>%
   filter(buf_m == 320 & bzone_m == 16000 & statefp != 'NA') %>% 
   rename(cat = conscat, medhhinc = emedhhinc) %>%
-  mutate(urban = if_else(urban == 'yes', 1, 0)) %>%
+  mutate(urban = if_else(urban == 'yes', 1, 0),
+         statefp = as.character(statefp)) %>%
   select(cat, statefp, sqkm_bz, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
   group_by(cat, statefp) %>%
-  summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = sum(urban), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
-            propPOC = mean(propPOC), medhhinc = mean(medhhinc), ) %>%
+  summarise(sd_sqkm = sd(sqkm_bz), sd_tot_pop = sd(tot_pop), sd_popden = sd(popden), sd_pwhite = sd(pwhite), sd_pblack = sd(pblack), 
+            sd_pother = sd(pother), sd_platinx = sd(platinx), sd_propPOC = sd(propPOC), sd_medhhinc = sd(medhhinc),
+            sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), 
+            pother = mean(pother), platinx = mean(platinx), propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
+  # summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
+  #           propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
   as.data.frame()
 
 ## GA and SC reserve data by urban
@@ -43,8 +48,12 @@ cl_demg_urb <- read.csv(file.path(datadir, 'cl_buf_demg_data.csv')) %>%
   mutate(statefp = if_else(urban == 'yes', 'Urban', 'Rural'), urban = if_else(urban == 'yes', 1, 0)) %>%
   select(cat, statefp, sqkm_bz, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
   group_by(cat, statefp) %>%
-  summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
-            propPOC = mean(propPOC), medhhinc = mean(medhhinc), ) %>%
+  summarise(sd_sqkm = sd(sqkm_bz), sd_tot_pop = sd(tot_pop), sd_popden = sd(popden), sd_pwhite = sd(pwhite), sd_pblack = sd(pblack), 
+            sd_pother = sd(pother), sd_platinx = sd(platinx), sd_propPOC = sd(propPOC), sd_medhhinc = sd(medhhinc),
+            sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), 
+            pother = mean(pother), platinx = mean(platinx), propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
+  # summarise(sqkm = mean (sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
+  #           propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
   as.data.frame()
 
 ## combine GA and SC reserve data
@@ -54,14 +63,19 @@ cl_demg_entire <- read.csv(file.path(datadir, 'cl_buf_demg_data.csv')) %>%
   mutate(urban = if_else(urban == 'yes', 1, 0)) %>%
   select(cat, statefp, sqkm_bz, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc) %>%
   group_by(cat) %>%
-  summarise(statefp = 'Lowcountry', sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = sum(urban), pwhite = mean(pwhite), pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
-            propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
+  summarise(statefp = 'Lowcountry', sd_sqkm = sd(sqkm_bz), sd_tot_pop = sd(tot_pop), sd_popden = sd(popden), sd_pwhite = sd(pwhite), sd_pblack = sd(pblack), 
+            sd_pother = sd(pother), sd_platinx = sd(platinx), sd_propPOC = sd(propPOC), sd_medhhinc = sd(medhhinc),
+            sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = n(), pwhite = mean(pwhite), pblack = mean(pblack), 
+            pother = mean(pother), platinx = mean(platinx), propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
+  # summarise(statefp = 'Lowcountry', sqkm = mean(sqkm_bz), tot_pop = mean(tot_pop), popden = mean(popden), urban = sum(urban), pwhite = mean(pwhite), 
+  #           pblack = mean(pblack), pother = mean(pother), platinx = mean(platinx), 
+  #           propPOC = mean(propPOC), medhhinc = mean(medhhinc)) %>%
   as.data.frame()
 
 ## GA and SC lowcountry data by state
 lc <- read.csv(file.path(datadir, 'lowcountry-by-state_data.csv')) %>%
   rename(medhhinc = emedhhinc, sqkm = sqkm_land) %>%
-  mutate(cat = 'Lowcountry', urban = NA) %>%
+  mutate(cat = 'Lowcountry', urban = NA, statefp = as.character(statefp)) %>%
   select(cat, statefp, sqkm, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc)
 
 ## combine GA and SC lowcountry data
@@ -70,16 +84,10 @@ lc_entire <- read.csv(file.path(datadir, 'lowcountry_data.csv')) %>%
   mutate(cat = 'Lowcountry', statefp = 'Lowcountry', urban = NA) %>%
   select(cat, statefp, sqkm, tot_pop, popden, urban, pwhite, pblack, pother, platinx, propPOC, medhhinc)
 
+df2 <- bind_rows(cl_demg, cl_demg_entire, lc, lc_entire, cl_demg_urb) %>%
+  mutate_if(is.numeric, round, 2)
 
-df2 <- rbind(cl_demg, cl_demg_entire)
-
-df3 <- rbind(df2, lc)
-
-df4 <- rbind(df3, lc_entire)
-
-df5 <- rbind(df4, cl_demg_urb)
-
-df6 <- df5 %>%
+df3 <- df2 %>%
   as.data.table() %>%
   select(cat:medhhinc) %>%
   mutate(region = if_else(statefp == 45, 'South Carolina', 
@@ -87,13 +95,12 @@ df6 <- df5 %>%
          sqkm = round(sqkm, 0),
          tot_pop = round(tot_pop, 0),
          popden = round(popden, 0),
-         pwhite = round(pwhite, 2),
-         pblack = round(pblack, 2),
-         platinx = round(platinx, 2),
-         pother = round(pother, 2),
-         propPOC = round(propPOC, 2),
-         medhhinc = round(medhhinc, 0)) %>%
+         medhhinc = round(medhhinc, 0),
+         sd_sqkm = round(sd_sqkm, 0),
+         sd_tot_pop = round(sd_tot_pop, 0),
+         sd_popden = round(sd_popden, 0),
+         sd_medhhinc = round(sd_medhhinc, 0)) %>%
   select(-statefp)
 
-write.csv(df6, file.path(datadir, 'regional-comparison.csv'))
+write.csv(df3, file.path(datadir, 'regional-comparison.csv'))
 
